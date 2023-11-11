1dd45d2dd45783196129293d3ef4eec99b41ae01
esteban <estebandebole@gmail.com>
2023-11-10 23:45:54 -0300

    Initial commit

---
+++ lib/delayed_job_uniq.rb
@@ -0,0 +1,6 @@
+require "delayed_job_uniq/version"
+
+module DelayedJobUniq
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/delayed_job_uniq/version.rb
@@ -0,0 +1,3 @@
+module DelayedJobUniq
+  VERSION = "0.1.0"
+end
